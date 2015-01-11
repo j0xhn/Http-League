2cf3a143089a9d96cc3870273a53b6dc43da556f
John D. Storey <johndangerstorey@gmail.com>
2015-01-10 20:35:38 -0700

    Re-styles login and signup page for Facebook

--- controllers/user.js
+++ controllers/user.js
@@ -68,7 +68,7 @@ exports.logout = function(req, res) {
 exports.getSignup = function(req, res) {
   if (req.user) return res.redirect('/');
   res.render('account/signup', {
-    title: 'Create Account'
+    title: 'Signup'
   });
 };
 
