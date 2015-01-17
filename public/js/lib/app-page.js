fe8190b4c659a2e082526be53b15b5e2ed2c76f7
John D. Storey <johndangerstorey@gmail.com>
2015-01-17 03:53:54 -0700

    Adds background color to body, adds in "report-score" tab.

--- public/js/lib/app-page.js
+++ public/js/lib/app-page.js
@@ -1,6 +1,18 @@
 
 // item elements
-$('.item').on({
+$('.item .person img, .item .person .name').on({
+    click: function () {
+        $(this).parent().parent().toggleClass('clicked');
+    }
+});
+
+$('.item .about').on({
+    click: function () {
+        $(this).parent().toggleClass('clicked');
+    }
+});
+
+$('.item .record-score').on({
     click: function () {
         $(this).toggleClass('clicked');
     }
